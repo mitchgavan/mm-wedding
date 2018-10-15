0b3e58a11190f7dd3ee12ca5aaa4349925e3fe5d
Mitch Gavan <mitchgavan@gmail.com>
2018-10-16 07:48:19 +1100

    feat: header content style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,17 @@ export default function Header() {
   return (
     <div className={styles.header}>
       <img src={hero} alt="" className={styles.bgImage} />
-      <h1 className={styles.title}>
-        Mitch <span className={styles.titleHighlight}>&</span> Melissa
-      </h1>
+      <header className={styles.content}>
+        <h1 className={styles.subTitle}>
+          We're getting married
+        </h1>
+        <h2 className={styles.title}>
+          Mitch <span className={styles.titleHighlight}>&</span> Melissa
+        </h2>
+        <p className={styles.date}>
+          16/03/2018
+        </p>
+      </header>
     </div>
   )
 }
