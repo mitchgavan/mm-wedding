c335641d21982c9ee6a7c87578f3aec4e6bd55de
Mitch Gavan <mitchgavan@gmail.com>
2018-10-25 07:30:11 +1100

    feat: decorative dividers

--- src/components/ImagePanel/ImagePanel.js
+++ src/components/ImagePanel/ImagePanel.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import imgAmsterdam from '../../images/mm2.jpg'
-import imgYounger from '../../images/mm3.jpg'
-import imgWedding from '../../images/mm1.jpg'
+import imgAmsterdam from '../../images/mm2.jpg';
+import imgYounger from '../../images/mm3.jpg';
+import imgWedding from '../../images/mm1.jpg';
+import imgFlowerDivider from '../../images/flower-divider.png';
 import styles from './ImagePanel.module.css'; 
 
 export default function ImagePanel() {
   return (
     <div className={styles.imagePanel}>
+      <img src={imgFlowerDivider} className={styles.divider} alt="" />
       <div className={styles.imagePanelScroll}>
         <div>
           <img src={imgYounger} className={styles.image} alt="Mitch and Mel younger" />
@@ -18,6 +20,7 @@ export default function ImagePanel() {
           <img src={imgWedding} className={styles.image} alt="Mitch and Mel Amsterdam" />
         </div>
       </div>
+      <img src={imgFlowerDivider} className={styles.divider} alt="" />
     </div>
   )
 }
