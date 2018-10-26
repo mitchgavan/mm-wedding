c1a19c52b524e1146c5915e1c1d49a12a55903db
Mitch Gavan <mitchgavan@gmail.com>
2018-10-26 19:11:24 +1100

    feat: smooth scroll

--- src/components/Header/TopBar/TopBar.js
+++ src/components/Header/TopBar/TopBar.js
@@ -3,10 +3,19 @@ import { ReactComponent as ArrowForward } from '../../../svg/arrow-forward.svg';
 import styles from './TopBar.module.css'; 
 
 export default function TopBar() {
+  const handleAnchorClick = (id) => (e) => {
+    const el = document.querySelector(`#${id}`);
+  
+    if (el && typeof el.scrollIntoView === 'function') {
+      e.preventDefault();
+      el.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+  
   return (
     <div className={styles.topbar}>
       <div>#MelAndMitchGetHitched</div>
-      <a href="#rsvp" className={styles.link}>
+      <a href="#rsvp" className={styles.link} onClick={handleAnchorClick('rsvp')}>
         <span>RSVP</span> <ArrowForward />
       </a>
     </div>
