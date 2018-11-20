07c8cc4b322fd0db444a6564d592e891fb1492b2
Mitch Gavan <mitchgavan@gmail.com>
2018-11-21 07:47:50 +1100

    feat: stripe basic integration

--- src/components/Registry/CheckoutForm.js
+++ src/components/Registry/CheckoutForm.js
@@ -1,30 +1,37 @@
 import React, { Component } from 'react';
 import { CardElement, injectStripe } from 'react-stripe-elements';
+import axios from 'axios';
 import styles from './CheckoutForm.module.css';
 
 class CheckoutForm extends Component {
-  submit = async () => {
-    console.log('submit');
-    // let {token} = await this.props.stripe.createToken({name: "Name"});
-    // let response = await fetch("/charge", {
-    //   method: "POST",
-    //   headers: {"Content-Type": "text/plain"},
-    //   body: token.id
-    // });
-  
-    // if (response.ok) console.log("Purchase Complete!")
+  state = {
+    status: 'default',
+  };
 
-    fetch('/.netlify/functions/hello')
-      .then(response => response.json())
-      .then(json => console.log(json.msg));
+  submit = async () => {
+    // let { token } = await this.props.stripe.createToken({ name: 'Name' });
+
+    try {
+      await axios.post('/.netlify/functions/charge', {
+        token: 'tok_au',
+      });
+      this.setState({ status: 'complete' });
+    } catch (err) {
+      this.setState({ status: 'error' });
+    }
   }
 
   render() {
+    const { status } = this.state;
+  
+    if (this.state.status === 'complete') return <h1>Purchase Complete</h1>;
+
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
         <CardElement />
         <button onClick={this.submit}>Send</button>
+        {status === 'error' && <div>Sorry, something went wrong.</div>}
       </div>
     )
   }
---
+++ src/lambda/charge.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const data = JSON.parse(event.body);
+
+  let { status } = await stripe.charges.create({
+    amount: 100,
+    currency: 'aud',
+    description: 'An example charge',
+    source: data.token
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ status })
+  };
+};
