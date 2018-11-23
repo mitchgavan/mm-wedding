3543b6f78cafd76b3759658e0419ea1125d8b663
Mitch Gavan <mitchgavan@gmail.com>
2018-11-24 08:02:52 +1100

    feat: update bride and groom section

--- src/components/ImagePanel/ImagePanel.js
+++ src/components/ImagePanel/ImagePanel.js
@@ -1,26 +1,40 @@
 import React from 'react';
+import Heading from '../Heading/Heading';
 import imgWedding from '../../images/mm2.jpg';
 import imgYounger from '../../images/mm3.jpg';
 import imgParis from '../../images/mm-paris.jpg';
 import { ReactComponent as FlowerDivider } from '../../svg/flower-divider.svg';
-import styles from './ImagePanel.module.css'; 
+import styles from './ImagePanel.module.css';
 
 export default function ImagePanel() {
   return (
     <div className={styles.imagePanel}>
-      <FlowerDivider className={styles.divider} />
+      <Heading>The Bride & Groom</Heading>
+      <p>Melissa Lyon and Mitchell Gavan</p>
       <div className={styles.imagePanelScroll}>
         <div>
-          <img src={imgYounger} className={styles.image} alt="Mitch and Mel younger" />
+          <img
+            src={imgYounger}
+            className={styles.image}
+            alt="Mitch and Mel younger"
+          />
         </div>
         <div>
-          <img src={imgWedding} className={styles.image} alt="Mitch and Mel wedding" />
+          <img
+            src={imgWedding}
+            className={styles.image}
+            alt="Mitch and Mel wedding"
+          />
         </div>
         <div>
-          <img src={imgParis} className={styles.image} alt="Mitch and Mel Paris" />
+          <img
+            src={imgParis}
+            className={styles.image}
+            alt="Mitch and Mel Paris"
+          />
         </div>
       </div>
       <FlowerDivider className={styles.divider} />
     </div>
-  )
+  );
 }
