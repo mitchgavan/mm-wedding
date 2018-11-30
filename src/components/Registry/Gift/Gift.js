f916fbf3f173acd58a5c8b54534b6819086c330c
Mitch Gavan <mitchgavan@gmail.com>
2018-11-30 18:56:20 +1100

    feat: gift images and style

--- src/components/Registry/Gift/Gift.js
+++ src/components/Registry/Gift/Gift.js
@@ -4,11 +4,16 @@ import styles from './Gift.module.css';
 
 export default class Gift extends Component {
   render() {
-    const { isSelected, title } = this.props;
+    const { image, isSelected, title } = this.props;
 
     return (
       <div className={cx(styles.gift, { [styles.selected]: isSelected })}>
-        <h3>{title}</h3>
+        <div className={styles.image}>
+          <img src={image} alt={title} />
+        </div>
+        <div className={styles.body}>
+          <h3 className={styles.title}>{title}</h3>
+        </div>
       </div>
     );
   }
--- src/components/Registry/Gifts/Gifts.js
+++ src/components/Registry/Gifts/Gifts.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
 import styles from './Gifts.module.css';
 import Gift from '../Gift/Gift';
+import imgFlights from '../../../images/gifts/flights@2x.jpg';
+import imgAccom from '../../../images/gifts/bali-accom@2x.jpg';
+import imgRestaurant from '../../../images/gifts/bali-restaurant@2x.jpg';
+import imgSpa from '../../../images/gifts/bali-spa@2x.jpg';
+import imgSightseeing from '../../../images/gifts/bali-sightseeing@2x.jpg';
+import imgMarkets from '../../../images/gifts/bali-markets@2x.jpg';
 
 const gifts = [
-  'Flights to Bali',
-  'Bali Resort Accomodation',
-  'Dinner and Drinks',
-  'Traditional Balinese Spa Package',
-  'Sightseeing Tour',
-  'Bintang Singlets and Ray-Bans',
+  {
+    title: 'Flights to Bali',
+    image: imgFlights,
+  },
+  {
+    title: 'Bali Resort Accommodation',
+    image: imgAccom,
+  },
+  {
+    title: 'Dinner and Drinks',
+    image: imgRestaurant,
+  },
+  {
+    title: 'Traditional Balinese Spa Package',
+    image: imgSpa,
+  },
+  {
+    title: 'Sightseeing Tour',
+    image: imgSightseeing,
+  },
+  {
+    title: 'Bintang Singlets and Ray-Bans',
+    image: imgMarkets,
+  },
 ];
 
 export default class Gifts extends Component {
@@ -18,9 +42,16 @@ export default class Gifts extends Component {
     return (
       <div className={styles.gifts}>
         {gifts.map(gift => (
-          <div className={styles.giftContainer} key={gift}>
-            <button className={styles.gift} onClick={() => onGiftSelect(gift)}>
-              <Gift title={gift} isSelected={selectedGift === gift} />
+          <div className={styles.giftContainer} key={gift.title}>
+            <button
+              className={styles.gift}
+              onClick={() => onGiftSelect(gift.title)}
+            >
+              <Gift
+                title={gift.title}
+                image={gift.image}
+                isSelected={selectedGift === gift.title}
+              />
             </button>
           </div>
         ))}
