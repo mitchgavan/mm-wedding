1a724aa1f909fa07f39313d7c5bfdc145c1ba734
Mitch Gavan <mitchgavan@gmail.com>
2018-12-05 18:34:56 +1100

    feat: accommodation details

--- src/components/Accommodation/Accommodation.js
+++ src/components/Accommodation/Accommodation.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 import styles from './Accommodation.module.css';
 import { ReactComponent as ArrowForward } from '../../svg/arrow-forward.svg';
-import imgFlights from '../../images/gifts/flights@2x.jpg';
 
 const hotels = [
   {
-    name: 'RACV Club Healesville',
-    address: '10 Stanley Rd, Healesville',
-    url: 'http://google.com',
-    image: imgFlights,
+    name: 'RACV Country Club',
+    address: 'Healesville',
+    url:
+      'https://www.tripadvisor.com.au/Hotel_Review-g552181-d2386523-Reviews-RACV_Healesville_Country_Club-Healesville_Yarra_Valley_Victoria.html',
   },
   {
-    name: 'RACV Club Healesville',
-    address: '10 Stanley Rd, Healesville',
-    url: 'http://google.com',
-    image: imgFlights,
+    name: 'Sanctuary House Resort Motel',
+    address: 'Healesville',
+    url:
+      'https://www.tripadvisor.com.au/Hotel_Review-g552181-d324361-Reviews-Sanctuary_House_Resort_Motel-Healesville_Yarra_Valley_Victoria.html',
   },
   {
-    name: 'RACV Club Healesville',
-    address: '10 Stanley Rd, Healesville',
-    url: 'http://google.com',
-    image: imgFlights,
+    name: 'Yarra Gables Motel',
+    address: 'Healesville',
+    url:
+      'https://www.tripadvisor.com.au/Hotel_Review-g552181-d2040886-Reviews-Yarra_Gables_Motel-Healesville_Yarra_Valley_Victoria.html',
   },
 ];
 
@@ -28,11 +27,10 @@ export default class Accommodation extends Component {
   render() {
     return (
       <div className={styles.accommodation}>
-        <p>Coming soon...</p>
+        <p>If you're looking for accommodation, here are a few ideas:</p>
         <div className={styles.options}>
           {hotels.map(hotel => (
             <div key={hotel.name} className={styles.option}>
-              <img src={hotel.image} alt={hotel.name} />
               <h3 className={styles.name}>{hotel.name}</h3>
               <div className={styles.address}>{hotel.address}</div>
               <a
@@ -41,7 +39,7 @@ export default class Accommodation extends Component {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <span>Visit website</span> <ArrowForward />
+                <span>View on TripAdvisor</span> <ArrowForward />
               </a>
             </div>
           ))}
